6ee5632041c702dad19345fce6e400a1bb429cf8
Dmitry Kekelidze <dmitry.kekelidze@cern.ch>
2025-04-08 17:14:31 +0200

    Replace DummyRepository with SmartRepository in DummyService
    Updated the DummyService class to use SmartRepository instead of DummyRepository. This improves the service's functionality by leveraging the advanced capabilities of SmartRepository.

--- src/main/java/cern/pori/java/service/DummyService.java
+++ src/main/java/cern/pori/java/service/DummyService.java
@@ -1,13 +1,14 @@
 package cern.pori.java.service;
 
 import cern.pori.java.repository.Repository;
+import cern.pori.java.repository.SmartRepository;
 
 public class DummyService {
 
   private final Repository repository;
 
   public DummyService() {
-    this.repository = new DummyRepository();
+    this.repository = new SmartRepository();
   }
 
   public String getHello() {
