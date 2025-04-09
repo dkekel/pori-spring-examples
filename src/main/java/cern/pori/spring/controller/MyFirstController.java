6a8897ece4f3bc7a7527a9062155f73112e7664f
Dmitry Kekelidze <dmitry.kekelidze@cern.ch>
2025-04-09 12:23:05 +0200

    Add REST API end-point

--- src/main/java/cern/pori/spring/controller/MyFirstController.java
+++ src/main/java/cern/pori/spring/controller/MyFirstController.java
@@ -1,6 +1,9 @@
 package cern.pori.spring.controller;
 
+import cern.pori.spring.model.DummyEntity;
 import cern.pori.spring.service.SpringCampusService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,4 +30,9 @@ public class MyFirstController {
     return "smart-hello";
   }
 
+  @GetMapping(path = "/api/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<DummyEntity> helloRest() {
+    DummyEntity helloEntity = springCampusService.getHelloEntity();
+    return ResponseEntity.ok(helloEntity);
+  }
 }
