3239e2d6bf2735247d3ec6779b60e2b588418bb1
Dmitry Kekelidze <dmitry.kekelidze@cern.ch>
2025-04-22 12:05:18 +0200

    Enable method-level security in SecurityConfiguration
    Added the @EnableMethodSecurity annotation to allow method-level security annotations such as @PreAuthorize and @PostAuthorize. This change enhances the flexibility and granularity of access control within the application.

--- src/main/java/cern/pori/spring/configuration/SecurityConfiguration.java
+++ src/main/java/cern/pori/spring/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import cern.pori.spring.security.JwtAuthenticationEntryPoint;
 import cern.pori.spring.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
